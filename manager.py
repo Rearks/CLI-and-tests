155ee5876ede2884963976571b5572f887f24680
kraer <kraer@yandex.ru>
2025-07-27 22:04:57 +0300

    Добавлен функционал Переименования

--- manager.py
+++ manager.py
@@ -2,6 +2,8 @@ import argparse
 from Copy import copy_file
 from Delete import delete_file
 from Count import count_files_in_folder
+from Rename import rename_with_date
+
 
 def main():
     parser = argparse.ArgumentParser(description="Простой файловый менеджер")
@@ -20,6 +22,11 @@ def main():
     count_parser = subparsers.add_parser("count", help="Подсчитать файлы в папке")
     count_parser.add_argument("source", help="Папка для подсчета файлов")
 
+    # Команда: rename
+    rename_parser = subparsers.add_parser("rename", help="Переименовать файл(ы) с добавлением даты")
+    rename_parser.add_argument("source", help="Файл или папка для обработки")
+    rename_parser.add_argument("--recursive", action="store_true", help="Обрабатывать папку рекурсивно")
+
     args = parser.parse_args()
 
     if args.command == "copy":
@@ -33,6 +40,8 @@ def main():
     elif args.command == "count":
         print(f"Количество файлов: {count_files_in_folder(args.source)}")
 
+    elif args.command == "rename":
+        rename_with_date(args.source, recursive=args.recursive)
     else:
         parser.print_help()
 
